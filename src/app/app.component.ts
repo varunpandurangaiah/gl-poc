0746a23e4a72e99063f118f8abfe9418db1230e7
Varun Pandurangaiah <Varun.Pandurangaiah@gds.ey.com>
2020-11-10 20:52:38 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'glpoc';
+  title = 'AI Enabled COA';
+  hostUrl = 'http://128.199.29.157:5000';
+  //http://128.199.29.157:5000/getpath?q=land
+  searchName: string;
+  myData;
+  constructor(private http: HttpClient){
+  }
+
+  getPath(){
+    this.http.get<any>(this.hostUrl + '/getpath',
+    {
+      params: new HttpParams().set('q', this.searchName),
+      headers: new HttpHeaders()
+        .set('Access-Control-Allow-Headers', 'Content-Type')
+        .set('Access-Control-Allow-Methods', 'GET')
+        .set('Access-Control-Allow-Origin', '*'),
+    })
+    .subscribe(data => {
+      let res  = data.split("-->");
+      this.myData = res;
+      var myJsonString = JSON.stringify(res);
+
+      console.log(this.myData);
+
+      console.log(myJsonString);
+    });
+  }
+
+  reset(){
+this.searchName = null;
+this.myData = null;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from 'src/app/angular-material.module';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
